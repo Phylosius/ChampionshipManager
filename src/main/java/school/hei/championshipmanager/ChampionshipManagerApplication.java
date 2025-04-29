421ee79737de5ad59c3c653ee9483f0fc993bf0b
Phylosius <hei.phylosius@gmail.com>
2025-04-30 00:49:41 +0300

    chore(spring-boot): initialize spring boot project

---
+++ src/main/java/school/hei/championshipmanager/ChampionshipManagerApplication.java
@@ -0,0 +1,13 @@
+package school.hei.championshipmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionshipManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampionshipManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/hei/championshipmanager/ChampionshipManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.championshipmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionshipManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
