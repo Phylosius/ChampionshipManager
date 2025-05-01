1f743e6d43dc2f6bc432a3d7b1a04825d333ccf4
Phylosius <hei.phylosius@gmail.com>
2025-05-02 00:02:16 +0200

    chore(api): create endpoint for getting club's statistics (not implemented)

--- src/main/java/school/hei/championshipmanager/controller/ClubRestController.java
+++ src/main/java/school/hei/championshipmanager/controller/ClubRestController.java
@@ -1,5 +1,6 @@
 package school.hei.championshipmanager.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import school.hei.championshipmanager.dto.ClubRest;
@@ -57,12 +59,13 @@ public class ClubRestController {
      * Change players of the specific club
      * 
      * Provided players inside the requestBody erase the existing players inside the club.
-        In case player is detached from club, it is still possible to retrieve his individual statistics 
-        and collective statistics do not change. 
-        For example, the player has 10 goals for the season, even if he is not part of the club anymore, 
-        the club statistics do not change (goals scored).
-        Finally, he must not be inside the list of players of the club and cannot make actions anymore for the remaining matches, if the season is not yet finished.        
-        In case, one of existing players is still attached to a club, API return 400 BAD_REQUEST.
+     * In case player is detached from club, it is still possible to retrieve his individual statistics 
+     * and collective statistics do not change. 
+     * For example, the player has 10 goals for the season, even if he is not part of the club anymore, 
+     * the club statistics do not change (goals scored).
+     * Finally, he must not be inside the list of players of the club and cannot make actions anymore for the remaining matches, if the season is not yet finished.        
+     * In case, one of existing players is still attached to a club, API return 400 BAD_REQUEST.
+     * 
      * @param id identifier of the specific club
      * @param players New players list of the club
      * @return List of actual players of the specific club
@@ -73,5 +76,20 @@ public class ClubRestController {
         @RequestBody List<PlayerRest> players
     ) {
         return ResponseEntity.status(501).body("Not Implemented.");
+    }
+
+    /***
+     * Get all clubs statistics for a specific season year
+     * 
+     * @param seasonYear the year of the season to get statistics for
+     * @param hasToBeClassified if true the statistics will be ordered by rankings. if false, the statistics will be ordered by club name ASC 
+     * @return All club statistics for the specific season
+     */
+    @GetMapping("/statistics/{seasonYear}")
+    public ResponseEntity<?> getStatistics(
+        @PathVariable LocalDate seasonYear,
+        @RequestParam(required = false) Boolean hasToBeClassified
+    ) {
+        return ResponseEntity.status(501).body("Not implemented.");
     }
 }
