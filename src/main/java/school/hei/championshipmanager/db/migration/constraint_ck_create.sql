f414e4a5f12112520d27ab2ca732bffa93f59c70
Phylosius <hei.phylosius@gmail.com>
2025-05-06 15:12:56 +0300

    fix(db): correct check constraint expression

--- src/main/java/school/hei/championshipmanager/db/migration/constraint_ck_create.sql
+++ src/main/java/school/hei/championshipmanager/db/migration/constraint_ck_create.sql
@@ -1,5 +1,5 @@
 -- object: match_clubs_ck | type: CONSTRAINT --
 -- ALTER TABLE public.match DROP CONSTRAINT IF EXISTS match_clubs_ck CASCADE;
-ALTER TABLE public.match ADD CONSTRAINT match_clubs_ck CHECK (host_team_id <> guest_team_id);
+ALTER TABLE public.match ADD CONSTRAINT match_clubs_ck CHECK (home_club_id <> away_club_id);
 -- ddl-end --
 
